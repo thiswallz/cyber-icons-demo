4ee874872bf2ea2e98f533ad3bce3126ead62aa0
MBJ <thiswallz@gmail.com>
2022-11-20 12:12:05 -0300

    add sizes and custom colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import * as CyberIcons from 'cyber-icons-react'
-import { colors } from 'cyber-icons-react'
-import { DocumentPageSetup } from 'cyber-icons-react'
+import { colors, categories, iconsByCategory } from 'cyber-icons-react'
 
+function formatName(text: string) {
+  return text.replace(/\-/g, ' ')
+}
+
+const initialCustomColor = {
+  primary: '',
+  secondary: '',
+  border: ''
+}
 
 export default function Home() {
-  const icons = Object.keys(CyberIcons);
+  const [theme, setTheme] = useState('eclipse')
+  const [iconName, setIconName] = useState('SystemLockScreen')
+  const [search, setSearch] = useState('')
+  const [size, setSize] = useState(2.5)
+  const [customColor, setCustomColor] = useState(initialCustomColor)
 
-  console.log('colors', colors)
+  const CurrentIcon = (CyberIcons as any)[iconName]
 
   return (
     <div className={styles.container}>
@@ -19,126 +32,100 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className={`${styles.main}`}>
+
+        <div className="fixed top-5 left-5 flex gap-4 text-sm z-50">
+          <img width="30px" src="/logo.png" alt="VastJS" />
+          VastJS
+        </div>
+        <div className="absolute top-5 right-5 flex">
+          <img width="100px" src="/react-min.png" alt="React" />
+          <img width="100px" src="/vue-min.png" alt="Vue" />
+          <img width="100px" src="/angular-min.png" alt="Angular" />
+        </div>
         <div>
-          <div>Select Theme</div>
+          <h1 className={styles.title}>
+            Cyber Icons
+          </h1>
           <div>
+            TODO: copy svg, category,
+
+          </div>
+          <h2 className="text-xl text-center">Select Theme</h2>
+        </div>
+        <div className="w-full sticky top-0 bg-black/80">
+          <div className="flex justify-center m-5 mx-16 gap-4">
             {
-              Object.keys(colors).map((color, index) => <div key={index}>
+              Object.keys(colors).map((color, index) => <div className="flex-none flex flex-col items-center" key={index}>
                 <div>{color}</div>
-                <div>
-                  <CyberIcons.SystemLockScreen theme={color} />
+                <div className="hover:opacity-75 cursor-pointer">
+                  <CurrentIcon theme={color} size="4em" onClick={() => { setTheme(color); setCustomColor(initialCustomColor); }} />
                 </div>
               </div>)
             }
+            <div className="flex-none items-center flex flex-col">
+              <div>custom</div>
+              <div className="flex flex-col gap-1">
+                <input type="color" className="w-16 h-6" value={customColor.primary} onChange={(e) => setCustomColor({ ...customColor, primary: e.target.value })} />
+                <input type="color" className="w-16 h-6" value={customColor.secondary} onChange={(e) => setCustomColor({ ...customColor, secondary: e.target.value })} />
+                <input type="color" className="w-16 h-6" value={customColor.border} onChange={(e) => setCustomColor({ ...customColor, border: e.target.value })} />
+                <button className="w-16 h-8" onClick={() => setCustomColor(initialCustomColor)}>reset</button>
+              </div>
+            </div>
           </div>
+          <code className="flex justify-center">
+            {`<${iconName} theme="${theme}" ${customColor.primary ? 'primary="' + customColor.primary + '"' : ''} ${customColor.secondary ? 'secondary="' + customColor.secondary + '"' : ''} ${customColor.border ? 'border="' + customColor.border + '"' : ''} size={\`${size}em\`} />`}
+          </code>
         </div>
+        <input type="range" min="1" max="10" value={size} onChange={(e) => setSize(e.target.value)} step=".1" className="fixed top-1/2 left-0 z-10" />
+        <section className="m-10">
+          <div className="flex justify-center"><input placeholder="Search..." onChange={(e) => setSearch(e.target.value)} /></div>
+          {
+            Object.keys(iconsByCategory).map((category, index) => <div key={index}>
+              <h2 className="ml-10 text-xl capitalize my-4">{formatName(category)}</h2>
+              <section className="icons-section dark flex gap-4 flex-wrap justify-center">
+                {
+                  iconsByCategory[category]
+                    .filter(icon => icon.toLowerCase().includes(search.toLowerCase()))
+                    .map((icon, i) => {
+                      const S = (CyberIcons as any)[icon];
+                      return <div className="w-48 flex flex-col gap-2 justify-center items-center" key={i}>
+                        <S
+                          size={`${size}em`}
+                          theme={theme}
+                          className="hover:opacity-75 cursor-pointer"
+                          primary={customColor.primary}
+                          secondary={customColor.secondary}
+                          border={customColor.border}
+                          onClick={() => setIconName(icon)}
+                        />
+                        <div className="text-xs">{`<${icon} />`}</div>
+                      </div>
+                    })
+                }
+                <div className="item-empty w-48"></div>
+                <div className="item-empty w-48"></div>
+                <div className="item-empty w-48"></div>
+                <div className="item-empty w-48"></div>
+              </section>
 
-        <div className="container-sections smallest">
-          <section className="dark">
-            {
-              icons.map((s, i) => {
-                if (typeof (CyberIcons as any)[s] !== 'function') {
-                  return null;
-                }
-                const S = (CyberIcons as any)[s];
-                return <S
-                  key={i}
-                  size={'1.5em'}
-                  primary={'#de004e'}
-                  secondary={'#321450'}
-                  border={'#f887ff'}
+            </div>)
+          }
+        </section>
 
+        <section>
+        React Installation
 
-                />
-              })
-            }
-          </section>
-        </div>
-        <div className="container-sections">
-          <section className="dark">
-            {
-              icons.map((s, i) => {
-                if (typeof (CyberIcons as any)[s] !== 'function') {
-                  return null;
-                }
-                const S = (CyberIcons as any)[s];
-                return <S
-                  key={i}
-                  size={'2em'}
-                />
-              })
-            }
-          </section>
-          <section className="dark">
-            {
-              icons.map((s, i) => {
-                if (typeof (CyberIcons as any)[s] !== 'function') {
-                  return null;
-                }
-                const S = (CyberIcons as any)[s];
-                return <S
-                  key={i}
-                  size={'3em'}
-                  primary={'#024059'}
-                  secondary={'#980BBF'}
-                  border={'#F2E307'}
-                />
-              })
-            }
-          </section>
-          <section className="dark">
-            {
-              icons.map((s, i) => {
-                if (typeof (CyberIcons as any)[s] !== 'function') {
-                  return null;
-                }
-                const S = (CyberIcons as any)[s];
-                return <S
-                  key={i}
-                  size={'3em'}
-                  primary={'magenta'}
-                  secondary={'blue'}
-                  border={'cyan'}
-                />
-              })
-            }
-          </section>
-          <section className="dark">
-            {
-              icons.map((s, i) => {
-                if (typeof (CyberIcons as any)[s] !== 'function') {
-                  return null;
-                }
-                const S = (CyberIcons as any)[s];
-                return <S
-                  key={i}
-                  size={'3em'}
-                  primary={'white'}
-                  secondary={'white'}
-                  border={'blue'}
-                />
-              })
-            }
-          </section>
-        </div>
+        <code>
+        npm install --save @vastjs/cyber-icons-react
+        </code>
+
+        </section>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+     <footer className={styles.footer}>
+
+     </footer>
     </div>
   )
 }
